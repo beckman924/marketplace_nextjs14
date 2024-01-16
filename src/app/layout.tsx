66586e2ef0fabaa7b9aba10bb21b3517e42098c9
Braian Emmanuel Carabajal <braiancarabajal@gmail.com>
2024-01-16 19:36:52 -0300

    Added Navbar and Cart

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         className={cn("relative h-full font-sans antialiased", inter.className)}
       >
         <main className="relative flex flex-col min-h-screen">
+          <Navbar />
           <div className="flex-grow flex-1">{children}</div>
         </main>
       </body>
---
+++ src/components/NavItems.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+
+import { PRODUCT_CATEGORIES } from "@/config";
+import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
+
+const NavItems = () => {
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+  const isAnyOpen = activeIndex !== null;
+
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
+
+    document.addEventListener("keydown", handler);
+
+    return () => document.removeEventListener("keydown", handler);
+  }, [setActiveIndex]);
+
+  return (
+    <div className="flex gap-4 h-full" ref={navRef}>
+      {PRODUCT_CATEGORIES.map((category, index) => {
+        const handleOpen = () => {
+          setActiveIndex(activeIndex === index ? null : index);
+        };
+
+        const isOpen = activeIndex === index;
+
+        return (
+          <NavItem
+            key={category.value}
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            isAnyOpen={isAnyOpen}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default NavItems;
