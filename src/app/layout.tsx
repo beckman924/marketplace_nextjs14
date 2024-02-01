9dd5d9af8e2d8829fe0d9c66aa903468fc61e173
Braian Emmanuel Carabajal <braiancarabajal@gmail.com>
2024-02-01 11:23:21 -0300

    Set Sign-up and Sign-in pages and Email verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { cn } from "@/lib/utils";
 
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Providers from "@/components/Providers";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,9 +26,13 @@ export default function RootLayout({
         className={cn("relative h-full font-sans antialiased", inter.className)}
       >
         <main className="relative flex flex-col min-h-screen">
-          <Navbar />
-          <div className="flex-grow flex-1">{children}</div>
+          <Providers>
+            <Navbar />
+            <div className="flex-grow flex-1">{children}</div>
+          </Providers>
         </main>
+
+        <Toaster position="top-center" richColors />
       </body>
     </html>
   );
