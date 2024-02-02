52efa2186e63f48d16dbb3eb925dbb1dbb672664
Braian Emmanuel Carabajal <braiancarabajal@gmail.com>
2024-02-02 12:09:59 -0300

    Added Collections

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,6 +6,10 @@ import path from "path";
 import dotenv from "dotenv";
 
 import { Users } from "./collections/Users";
+import { Products } from "./collections/Products/Products";
+import { Media } from "./collections/Media";
+import { ProductFiles } from "./collections/ProductFile";
+import { Orders } from "./collections/Orders";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
@@ -13,7 +17,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [Users],
+  collections: [Users, Products, Media, ProductFiles, Orders],
   routes: {
     admin: "/sell",
   },
