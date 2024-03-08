86d6bbecf1427705ae47a3134fab27f0895b990c
Braian Emmanuel Carabajal <braiancarabajal@gmail.com>
2024-03-08 09:51:02 -0300

    Image Slider in product

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
       {
         protocol: "http",
         hostname: "localhost",
+        port: "3000",
+        pathname: "**",
       },
       {
         protocol: "https",
