4a90c3400998ac0560d5d923c2976f965960ea45
Braian Emmanuel Carabajal <braiancarabajal@gmail.com>
2024-03-09 12:54:32 -0300

    Added middleware to cart route, access rules for Products and Users,  PrimaryActionEmail and ProductsPage components

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,14 @@ import { inferAsyncReturnType } from "@trpc/server";
 import bodyParser from "body-parser";
 import { IncomingMessage } from "http";
 import nextBuild from "next/dist/build";
+import path from "path";
+import { PayloadRequest } from "payload/types";
+import { parse } from "url";
 
 import { getPayloadClient } from "./get-payload";
 import { nextApp, nextHandler } from "./next-utils";
 import { appRouter } from "./trpc";
 import { stripeWebhookHandler } from "./webhooks";
-import path from "path";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -37,14 +39,30 @@ const start = async () => {
 
   app.post("/api/webhooks/stripe", webhookMiddleware, stripeWebhookHandler);
 
-    const payload = await getPayloadClient({
-      initOptions: {
-        express: app,
-        onInit: async (cms) => {
-          cms.logger.info(`Admin URL ${cms.getAdminURL()}`);
-        },
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL ${cms.getAdminURL()}`);
       },
-    });
+    },
+  });
+
+  const cartRouter = express.Router();
+
+  cartRouter.use(payload.authenticate);
+
+  cartRouter.get("/", (req, res) => {
+    const request = req as PayloadRequest;
+
+    if (!request.user) return res.redirect("/sign-in?origin=cart");
+
+    const parsedUrl = parse(req.url, true);
+
+    return nextApp.render(req, res, "/cart", parsedUrl.query);
+  });
+
+  app.use("/cart", cartRouter);
 
   if (process.env.NEXT_BUILD) {
     app.listen(PORT, async () => {
